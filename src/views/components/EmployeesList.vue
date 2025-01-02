7294c5c5f3e33ec0e8de5343a45affd60c92a138
Alexsandr <82975768+Sminor01@users.noreply.github.com>
2025-01-03 02:59:58 +0300

    change

--- src/views/components/EmployeesList.vue
+++ src/views/components/EmployeesList.vue
@@ -2,6 +2,7 @@
     <div class="card">
       <div class="card-header pb-0">
         <h6>Список сотрудников</h6>
+        <h6>Список сотрудников</h6>
       </div>
       <div class="card-body px-0 pt-0 pb-2">
         <div class="table-responsive p-0">
@@ -12,15 +13,18 @@
                   class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
                 >
                   ФИО сотрудника
+                  ФИО сотрудника
                 </th>
                 <th
                   class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"
                 >
                   Отдел
+                  Отдел
                 </th>
                 <th
                   class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
                 >
+                  Должность
                   Должность
                 </th>
                 <th class="text-center text-uppercase text-xxs font-weight-border opacity-7 text-secondary opacity-7">
@@ -35,6 +39,7 @@
                     </div>
                     <div class="d-flex flex-column justify-content-center">
                       <h6 class="mb-0 text-sm">Пряхин А.С.</h6>
+                      <h6 class="mb-0 text-sm">Пряхин А.С.</h6>
                       <p class="text-xs text-secondary mb-0">
                         отчёт
                       </p>
@@ -44,10 +49,12 @@
                 <td>
                   <p class="text-xs font-weight-bold mb-0">Разработка ПО</p>
                   <p class="text-xs text-secondary mb-0">frontend</p>
+                  <p class="text-xs font-weight-bold mb-0">Разработка ПО</p>
+                  <p class="text-xs text-secondary mb-0">frontend</p>
                 </td>
                 <td class="align-middle text-center">
                   <span class="text-secondary text-xs font-weight-bold"
-                    >Помощник инженера-программиста</span
+                    >123</span
                   >
                 </td>
                 <td class="align-middle">
